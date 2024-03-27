b312378671e9726a89d8375354ef1643ca8fb30c
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-26 18:36:44 -0600

    Update Menubar component

--- src/Navegacion/Menubar.js
+++ src/Navegacion/Menubar.js
@@ -1,11 +1,13 @@
-import {  Header, Octicon,  } from "@primer/react";
-import {  SquirrelIcon } from "@primer/octicons-react";
-import {  Container, Toolbar, Typography, IconButton, Drawer, List,  Icon} from '@material-ui/core';
-import React, { useState } from 'react';
+import { Header, Octicon, Button, Overlay, Box} from "@primer/react";
+
+import {  ListUnorderedIcon, SquirrelIcon } from "@primer/octicons-react";
+import {  Container, Toolbar, Typography} from '@material-ui/core';
+import React, { useRef, useState } from 'react';
 import useStyles from '../Themes/useStyles';
 import { Link } from 'react-router-dom';
 import MenuCliente from "./Desktop/MenuCliente";
 import MenuMobile from "./Mobile/MenuMobile";
+
 
 
 
@@ -18,27 +20,53 @@ const MenuBar = () => {
     const closeToggle =() => {
         setOpen(false);
     }
-
+    const anchorRef = useRef(null);
+    const buttonRef = useRef(null);
+    const confirmRef = useRef(null);
+    
     return (
         <div>
             <Header sx={{color:'white', alignContent:'center'}} className={classes.Appbar} >
                  <Container >
                     <Toolbar>
-                            <div className={classes.sectionMobile}>
-                                <IconButton color ="inherit" onClick={openToggle}>
-                                    <Icon fontSize = "large">menu</Icon>
-                                </IconButton>
+                            <div ref={anchorRef} className={classes.sectionMobile}>
+                                <Button ref={buttonRef}
+                                    size="small"
+                                    sx={{
+                                        backgroundColor:'transparent',
+                                        size:50, 
+                                        textDecoration:'none', 
+                                        ':active':{ outline: 'none'},
+                                        ':foucus': { outline: 'none' }
+                                    }} 
+                                    variant="invisible"  
+                                    onClick={openToggle}
+                                >
+                                    <Octicon icon={ListUnorderedIcon} sx={{color:'whitesmoke'}} size={32}></Octicon>
+                                </Button>
+                                    {open ?  ( 
+                                    <Overlay initialFocusRef={confirmRef} returnFocusRef={buttonRef} ignoreClickRefs={[buttonRef]}
+                                     onClickOutside={closeToggle} anchorSide="inside-left"
+                                      width="130px" height="1080px" align="center" side="outside-bottom"
+                                      
+                                      >
+                                        <Box
+                                            sx={{
+                                                height: ['1080px', 'auto'], // 100vh en móviles, auto en otros dispositivos
+                                                width: ['100px', '500px'], // 100vw en móviles, 500px en otros dispositivos
+                                                display: 'flex',
+                                                color: 'white',
+                                                flexDirection: 'column',
+                                                
+                                            }}
+                                        >
+                                            <MenuMobile clickHandler={closeToggle}></MenuMobile>
+                                        </Box> 
+                                    </Overlay>
+                                    ) : null
+                                    }
                             </div>  
-                            <Drawer  open={open}
-                            onClose={closeToggle}>
-                                <div  className={classes.list}>
-                                    <List>
-                                       {/*  <MenuMobilePublic clickHandler={closeToggle}></MenuMobilePublic> */}
-                                        <MenuMobile clickHandler={closeToggle}/>
-                                    </List>
-                                </div>
-                                
-                            </Drawer>                  
+                                         
                             <div className={classes.grow}>   
                                 
                                 <Link color="inherit" to='/Inicio' className={classes.LinkBarLogo} underline="none">
