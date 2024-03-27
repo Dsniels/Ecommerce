6d991506a5dcd0cfdc06e41b085b98774f7dad60
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-26 23:59:06 -0600

    Add navigation functionality to mobile menu

--- src/Navegacion/Mobile/MenuMobile.js
+++ src/Navegacion/Mobile/MenuMobile.js
@@ -2,6 +2,8 @@ import React from 'react'
 import useStyles from '../../Themes/useStyles'
 import { Avatar, ListItem, } from "@material-ui/core";
 import { NavList } from '@primer/react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+
 
 
 export default function MenuMobile(props) {
@@ -10,7 +12,20 @@ export default function MenuMobile(props) {
     const handleClickCliente = () => {
         setOpenCliente((prevOpen) => !prevOpen);
     } */
+    const history = useHistory();
     const classes = useStyles();
+    const perfil = () => {
+        history.push("/Perfil");
+    };
+    const pedidos = () => {
+        history.push("/Pedidos");
+    };
+    const iniciarSesion = () => {
+        history.push("/IniciarSesion");
+    };
+    const tienda = () => {
+        history.push("/Tienda");
+    };
 
   return (
     <>
@@ -22,15 +37,15 @@ export default function MenuMobile(props) {
                             <Avatar style={{display:'flex', justifyContent:'center'}} size={20} className={classes.avatarPerfilAppBar} src="https://avatars.githubusercontent.com/u/92997159?v=4" />
 
                             <NavList.SubNav>
-                                <NavList.Item className={classes.NavLisMobileItem} > Mi Perfil</NavList.Item>
-                                <NavList.Item className={classes.NavLisMobileItem}> Mis Pedidos</NavList.Item>
+                                <NavList.Item onClick={perfil} className={classes.NavLisMobileItem} > Mi Perfil</NavList.Item>
+                                <NavList.Item onClick={pedidos} className={classes.NavLisMobileItem}> Mis Pedidos</NavList.Item>
                                 <NavList.Divider />
                                 <NavList.Item variant="danger"> Cerrar Sesion</NavList.Item>
                             </NavList.SubNav>
                         </NavList.Item>
-                        <NavList.Item className={classes.NavLisMobileItem}>Productos</NavList.Item>
+                        <NavList.Item onClick={tienda} className={classes.NavLisMobileItem}>Productos</NavList.Item>
                         <NavList.Item className={classes.NavLisMobileItem}> Contacto</NavList.Item>
-                        <NavList.Item className={classes.NavLisMobileItem}> Iniciar Sesion</NavList.Item>
+                        <NavList.Item onClick={iniciarSesion} className={classes.NavLisMobileItem}> Iniciar Sesion</NavList.Item>
                     </NavList>
                 </div>                
             </ListItem>
