d55eb82a5e6182d22701cbdb672d3c15fa03fe01
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-26 18:36:55 -0600

    Refactor MenuMobile component

--- src/Navegacion/Mobile/MenuMobile.js
+++ src/Navegacion/Mobile/MenuMobile.js
@@ -1,59 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import useStyles from '../../Themes/useStyles'
-import { Avatar, Collapse, Link, ListItem, ListItemIcon, ListItemText,Icon, List } from "@material-ui/core";
+import { Avatar, ListItem, } from "@material-ui/core";
+import { NavList } from '@primer/react';
+
 
 export default function MenuMobile(props) {
 
-    const[openCliente, setOpenCliente] = useState(false);
+/*     const[openCliente, setOpenCliente] = useState(false);
     const handleClickCliente = () => {
         setOpenCliente((prevOpen) => !prevOpen);
-    }
+    } */
     const classes = useStyles();
 
   return (
     <>
     
-            <ListItem button onClick={handleClickCliente} className={classes.listItem}>
+            <ListItem  className={classes.listItem}>
                 <div className={classes.LinkBarmobile}>
-                    <Avatar
-                    alt="imagen"
-                    className={classes.avatarPerfilAppBar}
-                    ></Avatar>
-                    <Icon> keyboard_arrow_down</Icon>
+                    <NavList className={classes.NavLisMobile}>
+                        <NavList.Item className={classes.NavLisMobileItem}>
+                            <Avatar style={{display:'flex', justifyContent:'center'}} size={20} className={classes.avatarPerfilAppBar} src="https://avatars.githubusercontent.com/u/92997159?v=4" />
+
+                            <NavList.SubNav>
+                                <NavList.Item className={classes.NavLisMobileItem} > Mi Perfil</NavList.Item>
+                                <NavList.Item className={classes.NavLisMobileItem}> Mis Pedidos</NavList.Item>
+                                <NavList.Divider />
+                                <NavList.Item variant="danger"> Cerrar Sesion</NavList.Item>
+                            </NavList.SubNav>
+                        </NavList.Item>
+                        <NavList.Item className={classes.NavLisMobileItem}>Productos</NavList.Item>
+                        <NavList.Item className={classes.NavLisMobileItem}> Contacto</NavList.Item>
+                        <NavList.Item className={classes.NavLisMobileItem}> Iniciar Sesion</NavList.Item>
+                    </NavList>
                 </div>                
             </ListItem>
-            <Collapse component="li" in={openCliente} timeout="auto" unmountOnExit>
-                <List disablePadding>
-                    <ListItem button className={classes.listSubItem} onClick={props.clickHandler}>
-                        <Link className={classes.LinkBarmobile} to="/perfil">
-                            <ListItemIcon className={classes.listItemIcon}>
-                                <Icon>person</Icon>
-                                <ListItem button onClick = '/perfil'>
-                                    <ListItemText>mi perfil</ListItemText>
-                                </ListItem>
-                            </ListItemIcon>
-                        </Link>
-                    </ListItem>
-                    <ListItem button className={classes.listSubItem} onClick={props.clickHandler}>
-                        <Link className={classes.LinkBarmobile} to="/">
-                            <ListItemIcon className={classes.listItemIcon}>
-                                <Icon>exit_to_app</Icon>
-                                <ListItem button > 
-                                    <ListItemText>cerrar sesion</ListItemText>
-                                </ListItem>
-                            </ListItemIcon>
-                        </Link>
-                    </ListItem>
-                </List>            
-            </Collapse>
-            <ListItem button className={classes.listItem} onClick={props.clickHandler}>
-                <Link className={classes.LinkBarmobile} to="/carrito">
-                    <ListItemIcon className={classes.listItemIcon}>
-                        <Icon>shopping_cart</Icon>
-                        <ListItemText>Mis Pedidos</ListItemText>
-                    </ListItemIcon>
-                </Link>
-            </ListItem>
+            
         </>
   )
 }
